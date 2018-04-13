dc44b0816f2d424880496511f4fc0b3b873d173a
RathiManoj <manojrathi25071997@gmail.com>
2018-04-13 07:09:47 +0530

    courses added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,38 @@
 window.addEventListener("load", init);
 
 function init() {
-    var login=document.getElementById("login").addEventListener("click", showOverlay);
+    
+    includeHTML();
 }
 
-
-function showOverlay() {
-    document.getElementById("overlay").style.display="block";
+function includeHTML() {
+    var z, i, elmnt, file,xhttp;
+    /* loop through a collection of all HTML elements */
+    z=document.getElementsByTagName("*");
+    for(i=0; i<z.length; i++) {
+        elmnt=z[i];
+        /* Search for elements with a certain attribute */
+        file = elmnt.getAttribute("include-html");
+        if (file) {
+            /* make a HTTP request using the attribute value as the file name */
+            xhttp=new XMLHttpRequest();
+            xhttp.onreadystatechange=function() {
+                if (this.readyState==4) {
+                    if (this.status==200) {
+                        elmnt.innerHTML=this.responseText;
+                    }
+                    if (this.status==404) {
+                        elmnt.innerHTML="Page Not Found!!";
+                    }
+                    /* Remove the attribute, and this function once more */
+                    elmnt.removeAttribute("include-html");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET",file, true);
+            xhttp.send();
+            /* exit the function */
+            return;
+        }
+    }
 }
